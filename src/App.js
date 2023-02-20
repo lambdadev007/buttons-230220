911b229d91f5cc5065b4460c4b5195b81f563be6
Lambda <poberezhetspavlo@gmail.com>
2023-02-20 05:48:44 -0500

    add the js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 import "./style/style.scss";
 
 function App() {
+  useEffect(()=> {
+    document.querySelectorAll('.grid-list').forEach(button => button.addEventListener('click', toggle));
+
+    function toggle() {
+        let btn = this;
+        btn.classList.add('animation');
+        btn.classList.toggle('active');
+        let newElem = btn.cloneNode(true);
+        btn.parentNode.replaceChild(newElem, btn);
+        newElem.addEventListener('click', toggle);
+    }
+
+  },[])
   return (
     <div className="App">
       <button className="grid-list without-text">
